7aed272542f704d4cb6f8248554196586374a601
Peter Yu <peteryu24@naver.com>
2020-07-03 17:08:27 +0900

    Three ways to print an ArrayList

--- ArrayList/ArrayListPrint.java
+++ ArrayList/ArrayListPrint.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class ArraylistPrint {
+public class ArrayListPrint {
 	public static void main(String[] args) {
 
 		ArrayList<Integer> arrlist = new ArrayList<>();
