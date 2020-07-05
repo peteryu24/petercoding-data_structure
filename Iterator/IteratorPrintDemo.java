9f4da9a0ebd25e01fcd5081bd45ee4bfb03d5421
Peter Yu <peteryu24@naver.com>
2020-07-05 17:34:07 +0900

    IteratorPrintDemo

--- Iterator/IteratorPrintDemo.java
+++ Iterator/IteratorPrintDemo.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class SimpleArraylist {
+public class IteratorPrintDemo {
 	public static void main(String[] args) {
 
 		ArrayList<Integer> arrlist = new ArrayList<>();
