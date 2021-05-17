6c417a7db99561b777e985b9c07c86fc8c92165f
Peter Yu <peteryu24@naver.com>
2021-05-17 18:00:04 +0900

    Works with CircularList2.java

---
+++ List/CircularLinkedList/ListNode.java
@@ -0,0 +1,35 @@
+package Circular;
+
+public class ListNode {
+	Object data;
+	ListNode link;
+
+	public ListNode() {
+	}
+
+	public ListNode(Object n) {
+		data = n;
+		link = null;
+	}
+
+	public ListNode(Object n, ListNode next) {
+		data=n;
+		link=next;
+	}
+
+	public void putData(Object n) {
+		data = n;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void putLink(ListNode next) {
+		link=next;
+	}
+
+	public ListNode getLink() {
+		return link;
+	}
+}
